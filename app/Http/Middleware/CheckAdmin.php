9ab903e9a0bc304f45cf6254dca6f7a256705e66
Sunly <sunlynai81@gmail.com>
2026-07-11 00:00:54 +0700

    done with authentication either member or admin

---
+++ app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->check() && auth()->user()->role === 1) {
+            return $next($request);
+        }
+
+        abort(403, 'Unauthorized action.');
+    }
+}
