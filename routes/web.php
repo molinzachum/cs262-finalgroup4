f9c66a5fb35ce1325e9f4ad75528f8d900be4173
ChanReaksa Chor <kzaa@ChanReaksas-MacBook-Pro.local>
2026-07-15 04:40:52 +0700

    Add shared header sidebar and footer

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ Route::middleware('auth')->group(function () {
 
     Route::resource('tasks', TaskController::class);
 
+    Route::view('/teams', 'team.index')->name('team.index');
+    Route::view('/time-logs', 'timelogs.index')->name('timelogs.index');
+
     Route::post('/tasks/{task}/assign',
         [TaskAssignmentController::class, 'store']
     );
